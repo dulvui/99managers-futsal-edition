42eb6ee49fb28dd9b647724431ab19330781a78c
Simon Dalvai <info@simondalvai.org>
2025-02-06 15:40:06 +0100

    translations: add continend and nation names

---
+++ game/src/resources/world/continent_names.gd
@@ -0,0 +1,18 @@
+# SPDX-FileCopyrightText: 2023 Simon Dalvai <info@simondalvai.org>
+
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+#
+# Kind of a hack to get Godot detect continent and nation names
+# so they get added to game.pot file for translations
+#
+
+class_name ContinentNames
+
+var africa: String = tr("AFRICA")
+var asia: String = tr("ASIA")
+var europe: String = tr("EUROPE")
+var oceania: String = tr("OCEANIA")
+var south_america: String = tr("SOUTH_AMERICA")
+var north_america: String = tr("NORTH_AMERICA")
+
