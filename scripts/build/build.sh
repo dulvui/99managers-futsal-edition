8907ab1d38acc68e03deb8f9a6cb1eb49ec1f182
Simon Dalvai <info@simondalvai.org>
2025-02-11 09:32:39 +0100

    scripts: build: fix zip archives

--- scripts/build/build.sh
+++ scripts/build/build.sh
@@ -36,20 +36,35 @@ mkdir builds/macos
 echo "Building Linux..."
 cp $GAME_PATH/export_presets.linux.example $GAME_PATH/export_presets.cfg
 $GODOT_PATH --headless --path $GAME_PATH --export-release "Linux/X11" "$PWD/builds/linux/$GAME_NAME-linux.x86_64"
-zip -r $PWD/builds/99managers-futsal-edition-linux.zip $PWD/builds/linux/
 echo "Building Linux done."
+
+echo "Creating Linux archive..."
+pushd builds/linux/
+zip -r ../../99managers-futsal-edition-linux.zip .
+popd
+echo "Creating Linux archive done."
 
 echo "Building Windows..."
 cp $GAME_PATH/export_presets.windows.example $GAME_PATH/export_presets.cfg
 $GODOT_PATH --headless --path $GAME_PATH --export-release "Windows Desktop" "$PWD/builds/windows/$GAME_NAME-windows.exe"
-zip -r $PWD/builds/99managers-futsal-edition-windows.zip $PWD/builds/windows/
 echo "Building Windows done."
+
+echo "Creating Windows archive..."
+pushd builds/windows/
+zip -r ../../99managers-futsal-edition-windows.zip .
+popd
+echo "Creating Windows archive done."
 
 echo "Building MacOS..."
 cp $GAME_PATH/export_presets.macos.example $GAME_PATH/export_presets.cfg
 $GODOT_PATH --headless --path $GAME_PATH --export-release "macOS" "$PWD/builds/macos/$GAME_NAME-macos.app"
-zip -r $PWD/builds/99managers-futsal-edition-macos.zip $PWD/builds/macos/
 echo "Building MacOS done."
+
+echo "Creating MacOS archive..."
+pushd builds/macos/
+zip -r ../../99managers-futsal-edition-macos.zip .
+popd
+echo "Creating MacOS archive done."
 
 echo "Cleaning up..."
 rm $GAME_PATH/export_presets.cfg
