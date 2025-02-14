216a5d7741ee3545bdfce893f20d997db00d3de9
Simon Dalvai <info@simondalvai.org>
2025-02-14 16:57:00 +0100

    json resource: fix from json parsing

--- game/src/resources/json_resource.gd
+++ game/src/resources/json_resource.gd
@@ -36,40 +36,41 @@ func to_json() -> Dictionary:
 
 
 func from_json(json: Dictionary) -> void:
-	var global_class_list: Array[Dictionary] = ProjectSettings.get_global_class_list()
-	# TODO move to global to not create on every parse
-
-	var json_resource_class_list: Array = (
-		global_class_list
-		. filter(
-			func(d: Dictionary) -> bool: return (
-				str(d.path).begins_with("res://") and d.base == "JSONResource"
-			)
-		)
-		. map(func(d: Dictionary) -> String: return d.class)
-	)
-
-	var json_resource_class_list_copy: Array = json_resource_class_list.duplicate(true)
-	while not json_resource_class_list_copy.is_empty():
-		json_resource_class_list_copy = (
-			global_class_list
-			. filter(
-				func(d: Dictionary) -> bool: return (
-					str(d.path).begins_with("res://") and d.base in json_resource_class_list_copy
-				)
-			)
-			. map(func(d: Dictionary) -> String: return d.class)
-		)
-		json_resource_class_list.append_array(json_resource_class_list_copy)
+	# var global_class_list: Array[Dictionary] = ProjectSettings.get_global_class_list()
+	# # TODO move to global to not create on every parse
+	#
+	# var json_resource_class_list: Array = (
+	# 	global_class_list
+	# 	. filter(
+	# 		func(d: Dictionary) -> bool: return (
+	# 			str(d.path).begins_with("res://") and d.base == "JSONResource"
+	# 		)
+	# 	)
+	# 	. map(func(d: Dictionary) -> String: return d.class)
+	# )
+	#
+	# var json_resource_class_list_copy: Array = json_resource_class_list.duplicate(true)
+	# while not json_resource_class_list_copy.is_empty():
+	# 	json_resource_class_list_copy = (
+	# 		global_class_list
+	# 		. filter(
+	# 			func(d: Dictionary) -> bool: return (
+	# 				str(d.path).begins_with("res://") and d.base in json_resource_class_list_copy
+	# 			)
+	# 		)
+	# 		. map(func(d: Dictionary) -> String: return d.class)
+	# 	)
+	# 	json_resource_class_list.append_array(json_resource_class_list_copy)
 
 	var property_list: Array[Dictionary] = get_property_list()
 	for property: Dictionary in property_list:
-		if property.usage == Const.CUSTOM_PROPERTY:
+		if property.usage == Const.CUSTOM_PROPERTY or property.usage == Const.CUSTOM_PROPERTY_EXPORT:
+			print(property)
 			var property_name: String = property.name
 			var property_value: Variant = get(property_name)
 			var json_value: Variant = json.get(property_name)
 
-			if _is_primitive(property):
+			if _is_primitive(property_name):
 				set(property_name, json_value)
 			elif typeof(property.type) == Variant.Type.TYPE_OBJECT:
 				if property_value is JSONResource:
@@ -85,11 +86,11 @@ func from_json(json: Dictionary) -> void:
 						if item is JSONResource:
 							var d: Dictionary = (item as JSONResource).to_json()
 							array.append(d)
-						#else:
-						#print("JSONResource error, array not supported object with name: " + property_name)
-				#dict[property_name] = array
-			#elif typeof(value) == Variant.Type.TYPE_COLOR:
-			#dict[property_name] = (value as Color).to_html()
+						else:
+							print("JSONResource error, array not supported object with name: " + property_name)
+				set(property_name, array)
+			elif typeof(property_value) == Variant.Type.TYPE_COLOR:
+				set(property_name, (json_value as Color).to_html())
 
 
 func _is_primitive(variant: Variant) -> bool:
