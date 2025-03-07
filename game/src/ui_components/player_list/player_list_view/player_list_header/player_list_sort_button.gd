435ddf40691c695ce344608e9dc6f9fdf7435708
Simon Dalvai <info@simondalvai.org>
2025-03-07 14:18:56 +0100

    player list: centr sort button text and polish filters

--- game/src/ui_components/player_list/player_list_view/player_list_header/player_list_sort_button.gd
+++ game/src/ui_components/player_list/player_list_view/player_list_header/player_list_sort_button.gd
@@ -4,8 +4,15 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
 class_name PlayerListSortButton
-extends DefaultButton
-
+extends Button
 
 @export var sort_key: String
 
+
+func _ready() -> void:
+	if tooltip_text.is_empty() and text.length() > 1:
+		tooltip_text = text
+
+
+func _pressed() -> void:
+	SoundUtil.play_button_sfx()
